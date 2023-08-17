adbac7ddb37945ce6cbe9b38afe2fb6704718457
gDoe24 <gregkeel11@gmail.com>
2023-08-17 08:00:15 -0400

    feat: eventhub

--- settings.sh
+++ settings.sh
@@ -1,3 +1,6 @@
+#! /bin/sh
+#chmod +x settings.sh
+#source settings.sh
 resourceGroup=udc-microservices-project
 functionApp=udc-project-functionapp
 storageAccount=udcprojectstorage1
@@ -6,9 +9,22 @@ region=eastus2
 cosmosDBAccountName="udacitycosmosdb"
 databaseName="udc-neighborly-db"
 ########################################
-# webApp=
+webApp=gregsudacityneighborlyapp
 ########################################
-# containerRegistry=
-# AKSCluster=
-# imageName=
-# imageTag=
+containerRegistry=udcneighborlyapp
+AKSCluster=UdcNeighborlyCluster
+imageName=udc-microservices-project
+imageTag=v1
+echo "=======Local Environment Variables======"
+echo "functionApp = "$functionApp
+echo "resourceGroup = "$resourceGroup
+echo "storageAccount = "$storageAccount
+echo "region = "$region
+echo "cosmosDBAccountName = "$cosmosDBAccountName
+echo "databaseName = "$databaseName
+echo "webApp = "$webApp
+echo "containerRegistry = "$containerRegistry
+echo "AKSCluster = "$AKSCluster
+echo "imageName = "$imageName
+echo "imageTag = "$imageTag
+echo "=======End of Result======"
